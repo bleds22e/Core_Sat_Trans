f2be157e30a51cb07cdc78efde503bcd3cc89752
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-03-08 11:27:41 -0500

    Read in data correctly and make column names consistent

--- script_with_functions.R
+++ script_with_functions.R
@@ -18,10 +18,31 @@ head(hj_andrews)
 shortgrass_steppe <- read.table("data/SGS_LTER_smammals.txt", header = TRUE, sep = "\t", na.strings = ".")
 head(shortgrass_steppe)
 
-# prep files to make them all similar
+# prep files
 
 jor_data <- select(jornada, year, season, habitat, web, spp, recap) %>% 
-            filter(recap != "Y", spp != "DIPO1", spp != "PERO1", spp != "NA") %>% 
-            group_by(spp, year, season, habitat, web) %>% 
-            arrange(spp, year, season, habitat, web)
+            filter(recap != "Y", spp != "DIPO1", spp != "PERO1", spp != "NA")
 
+hja_data <- select(hj_andrews, YEAR, REPBLK, SUBPLOT, SPECIES) %>% 
+            filter(SPECIES != 'UNKN') 
+
+sev_data <- select(sevilleta, year, season, location, web, species, recap) %>% 
+            filter(recap != "y", species != "pgsp", species != "dipo", 
+                   species != "nesp", species != "onsp", species != "pesp", 
+                   species != "resp", species != "na", species != "pmsp", species != "spsp", 
+                   year < 2009, location != "two22", location != "blugrama", 
+                   location != "savanna", location != "goatdraw")
+
+sgs_data <- select(shortgrass_steppe, YEAR, VEG, WEB, SPP) %>% 
+            filter(SPP != 'NA')
+
+# rename columns for consistency
+
+names(jor_data) <- c("year", "season", "plot", "subplot", "species", "recap")
+names(hja_data) <- c("year", "plot", "subplot", "species")
+names(sev_data) <- c("year", "season", "plot", "subplot", "species", "recap")
+names(sgs_data) <- c("year", "plot", "subplot", "species")
+
+# write function for correct grouping of data
+
+group_data
