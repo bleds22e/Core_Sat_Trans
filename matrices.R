eec9493e29e630fb06ed0cc5b0c44e1e3f0f4cb2
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-07-08 17:33:56 -0400

    sum row and columns of a matrix

--- matrices.R
+++ matrices.R
@@ -3,9 +3,11 @@
 # EKB
 
 install.packages("permute")
+library(permute)
 
-m <- 9
+m <- 6
 n <- 5
 hja_matrix <- matrix(sample(0:1, m * n, replace = TRUE), m, n)
-
-
+hja_matrix
+rowSums(hja_matrix)
+colSums(hja_matrix)
