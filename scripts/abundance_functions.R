524e933b1fce79ebbcc097256a017fb0937c814c
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-11-21 11:51:00 -0500

    back to original

--- scripts/abundance_functions.R
+++ scripts/abundance_functions.R
@@ -55,10 +55,10 @@ adj_avg_abund <- function(data){
 
 find_reg_persist <- function(data){
   # create column for relative regional persistence (years overall)
-  dat <- select(data, species, year, LTER) %>% 
+  dat <- select(data, species, year) %>% 
     group_by(species, year) %>% 
     summarise(year_count = n())
-  dat1 <- select(dat, species, LTER) %>% 
+  dat1 <- select(dat, species) %>% 
     group_by(species) %>% 
     summarise(years = n())
   total_years <- length(unique(data$year))
