b40bcc6be6b68c00cef5f37ca637078f975df22c
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-12-08 09:19:54 -0500

    new script and data for rodent life history traits

---
+++ scripts/life_history_traits.R
@@ -0,0 +1,19 @@
+# Life History Traits - Rodents
+  # for Multivariate stats project
+
+#######################
+# LIBRARIES
+
+library(dplyr)
+
+#######################
+# DATA
+
+pantheria <- read.table("data/Rodents/PanTHERIA.txt", header = TRUE, sep = "\t", na.strings = c("-999", "-999.00"))
+pantheria <- select(pantheria, MSW05_Order, MSW05_Genus, MSW05_Species, MSW05_Binomial, X5.1_AdultBodyMass_g, 
+                    X6.1_DietBreadth, X12.1_HabitatBreadth, X22.1_HomeRange_km2) %>% 
+             filter(MSW05_Order == 'Rodentia')
+pantheria <- rename(pantheria, Order = MSW05_Order, Genus = MSW05_Genus, Species = MSW05_Species, Binomial = MSW05_Binomial, 
+                    AdultBodyMass_g = X5.1_AdultBodyMass_g, DietBreadth = X6.1_DietBreadth, HabitatBreadth = X12.1_HabitatBreadth,
+                    HomeRange_km2 = X22.1_HomeRange_km2)
+
