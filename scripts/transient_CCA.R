e06c0685a69159fb48eb404beaa0a6ef4342c574
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2018-10-31 15:57:16 -0400

    Interpolate missing values to remove NAs, run CCA

--- scripts/transient_CCA.R
+++ scripts/transient_CCA.R
@@ -28,12 +28,15 @@ find_reg_persist <- function(data){
 # GET DATA #
 
 rdat <- read.csv(text = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent.csv"), na.strings = '')
+trapping <- read.csv(text = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_trapping.csv"))
 NDVI <- read.csv(text = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/NDVI/monthly_NDVI.csv"))
 
 #===============================================================
 
+# get rodent abundance datat
 abund <- portalr::abundance(path = "repo", level = "Site", type = "Rodents",
-                            plots = "all", unknowns = F, shape = "flat", time = "period")
+                            plots = "all", unknowns = F, shape = "flat", 
+                            time = "period", min_plots = 10)
 
 ### ALERT ###
 
@@ -90,7 +93,7 @@ transients3 <- comm3_persist %>%
 transients3 <- unlist(apply(transients3, 1, list), recursive = FALSE)
 
 
-# GET ABUNDANCES #
+### GET ABUNDANCES ###
 
 # get total abundance by community
 total_by_period1 <- comm1 %>% group_by(period) %>% 
@@ -131,10 +134,36 @@ dates <- select(rdat, year, month, period) %>%
   distinct()
 dates$period <- as.integer(dates$period)
 
+abund_dates1 <- inner_join(rel_abund_transients1, dates, by = 'period') 
+abund_dates2 <- inner_join(rel_abund_transients2, dates, by = 'period') 
+abund_dates3 <- inner_join(rel_abund_transients3, dates, by = 'period') 
+abund_dates <- bind_rows(abund_dates1, abund_dates2, abund_dates3)
+
+# ADD PLOTS TRAPPED #
+
+# get data on how many plots were trapped each period
+plots_trapped <- trapping %>% 
+  select(period, plot, sampled) %>% 
+  group_by(period) %>% 
+  summarise(count_plots = sum(sampled))
+
+abund_dates <- left_join(abund_dates, plots_trapped, by = "period") 
+abund_dates <- filter(abund_dates, date < 2014)
+
+# if number of plots trapped is between 10 and 17, double the transients
+for (i in 1:nrow(abund_dates)){
+  if (abund_dates$count_plots[i] <= 17){
+    abund_dates$transient_rel_abund[i] <- abund_dates$transient_rel_abund[i]*2
+  }
+}
+
+
+### NDVI ###
+
 # make a date column for NDVI
 NDVI <- arrange(NDVI, year, month)
 
-for (i in 1:length(NDVI$month)){
+for (i in 1:nrow(NDVI)){
   if (str_length(NDVI$month[i]) == 1){
     NDVI$month[i] = paste('0', NDVI$month[i], sep = '')
   }
@@ -144,18 +173,17 @@ NDVI <- select(NDVI, year, month, ndvi) %>%
   tidyr::unite(date, year, month, sep = '-') %>% 
   distinct()
 
-# filter NDVI by community dates
-abund_dates1 <- inner_join(rel_abund_transients1, dates, by = 'period') 
-abund_dates2 <- inner_join(rel_abund_transients2, dates, by = 'period') 
-abund_dates3 <- inner_join(rel_abund_transients3, dates, by = 'period') 
-abund_dates <- bind_rows(abund_dates1, abund_dates2, abund_dates3)
-
+# make a column for NDVI centered around the median
 NDVI_peak <- mutate(NDVI, NDVIpeak = ndvi - median(ndvi))
 
+# filter NDVI by community dates
 NDVI1 <- filter(NDVI_peak, date %in% unique(abund_dates1$date))
 NDVI2 <- filter(NDVI_peak, date %in% unique(abund_dates2$date))
 NDVI3 <- filter(NDVI_peak, date %in% unique(abund_dates3$date))
 NDVI_all <- bind_rows(NDVI1, NDVI2, NDVI3)
+
+
+### COMBINE TRANSIENTS AND NDVI ###
 
 combined_data <- full_join(abund_dates, NDVI_all, by = "date")
 combined_data$date = as.yearmon(combined_data$date)
@@ -169,15 +197,12 @@ combined_data <- combined_data[!duplicated(combined_data$period),]
 all_dates <- as.data.frame(seq(as.yearmon(min(NDVI_peak$date)), as.yearmon(max(NDVI_peak$date)), by = 1/12))
 colnames(all_dates) = c("date")
 
-combined_data <- full_join(combined_data, all_dates, by = "date")
-combined_data <- combined_data[-c(1:30),]
-
-# quantiles
-upper <- as.numeric(quantile(NDVI_peak$NDVIpeak, .75))
-lower <- as.numeric(quantile(NDVI_peak$NDVIpeak, .25))
+combined_data <- full_join(combined_data, all_dates, by = "date") %>% 
+  arrange(date)
+combined_data <- combined_data[-c(1:65),]
 
 # add in NDVI values even if there aren't transient values
-for (i in 1:length(combined_data$date)){
+for (i in 1:nrow(combined_data)){
   
   # Is the value NA? If so, save the date as object 'date'
   if (is.na(combined_data$ndvi[i])){
@@ -185,7 +210,7 @@ for (i in 1:length(combined_data$date)){
     
     # Is it true that the date is in the NDVI_all dataframe?
     # If so, replace the NAs in the ndvi column and NDVIpeak columns with values
-    # from the NDVI_all dataframe
+    #     from the NDVI_all dataframe
     if (length(which(NDVI_all$date == date))>0){
       combined_data$ndvi[i] <- NDVI_all[NDVI_all$date == date, "ndvi"]
       combined_data$NDVIpeak[i] <- NDVI_all[NDVI_all$date == date, "NDVIpeak"]
@@ -194,9 +219,35 @@ for (i in 1:length(combined_data$date)){
   }
 }
 
+
+# INTERPOLATE MISSING VALUES #
+
+for (i in 1:nrow(combined_data)){
+
+  # fill in missing transient values
+  if (is.na(combined_data$transient_rel_abund[i])){
+    combined_data$transient_rel_abund[i] <- (combined_data$transient_rel_abund[i-1] + combined_data$transient_rel_abund[i+1])/2
+  }
+  
+  # fill in missing NDVI values
+  if (is.na(combined_data$NDVIpeak[i])){
+    combined_data$NDVIpeak[i] <- (combined_data$NDVIpeak[i-1] + combined_data$NDVIpeak[i+1])/2
+  } 
+  
+}
+
+combined_data$transient_rel_abund[165:166] <- 0
+combined_data$NDVIpeak[165:166] <- (combined_data$NDVIpeak[164] + combined_data$NDVIpeak[167])/2
+
+# FIND NDVI PEAKS #
+
+# quantiles
+upper <- as.numeric(quantile(NDVI_peak$NDVIpeak, .75))
+lower <- as.numeric(quantile(NDVI_peak$NDVIpeak, .25))
+
 combined_data$NDVI_quantiles = NA
 
-for (i in 1:length(combined_data$NDVIpeak)){
+for (i in 1:nrow(combined_data)){
   if (!is.na(combined_data$NDVIpeak[i])){
     if (combined_data$NDVIpeak[i] > upper){
       combined_data$NDVI_quantiles[i] = combined_data$NDVIpeak[i]
@@ -210,7 +261,7 @@ for (i in 1:length(combined_data$NDVIpeak)){
 combined_data$pulseID <- NA
 pulseID = 1
 
-for (i in 1:length(combined_data$date)){
+for (i in 1:nrow(combined_data)){
   
   if (!is.na(combined_data$NDVI_quantiles[i])){
     if(is.na(combined_data$pulseID[i-1])){
@@ -231,7 +282,7 @@ combined_pulses <- combined_data[!is.na(combined_data$pulseID),]
 nrows <- max(combined_pulses$pulseID, na.rm = TRUE)
 pulses <- data.frame(pulseID = integer(nrows),
                      pulseMax = numeric(nrows),
-                     pulseDuration = integer(nrows),
+                     pulseDuration = numeric(nrows),
                      pulseSum = numeric(nrows),
                      timeBetween = integer(nrows), # time from end of peak to start of next
                      transientT0 = numeric(nrows),
@@ -248,10 +299,10 @@ pulses <- data.frame(pulseID = integer(nrows),
                      transientT11 = numeric(nrows),
                      transientT12 = numeric(nrows))
 
-pulse_seq <- seq(from = 1, to = nrows)
+pulse_seq <- as.numeric(seq(from = 1, to = nrows))
 pulses$pulseID <- pulse_seq
 
-for (i in pulse_seq){
+for (i in 1:length(pulse_seq)){
   
   # select only rows from that pulse
   current_pulse <- combined_pulses[combined_pulses$pulseID == i,]
@@ -266,35 +317,41 @@ for (i in pulse_seq){
   pulses$pulseSum[i] <- sum(current_pulse$NDVIpeak)
   
   # find time (months) between end of current pulse and start of next pulse
-  pulses$timeBetween[i] <- 12 * (min(combined_pulses[combined_pulses$pulseID == i + 1, "date"]) - max(current_pulse$date))
+  pulses$timeBetween[i] <- 12 * (min(combined_pulses$date[combined_pulses$pulseID == i + 1]) - max(current_pulse$date))
   
   # find the transient rel abundances
   min_date <- min(current_pulse$date)
   min_row <- as.integer(rownames(combined_data[combined_data$date == min_date,]))
-  pulses$transientT0[i] <- combined_data[as.character(min_row), "transient_rel_abund"]
-  pulses$transientT1[i] <- combined_data[as.character(min_row + 1), "transient_rel_abund"]
-  pulses$transientT2[i] <- combined_data[as.character(min_row + 2), "transient_rel_abund"]
-  pulses$transientT3[i] <- combined_data[as.character(min_row + 3), "transient_rel_abund"]
-  pulses$transientT4[i] <- combined_data[as.character(min_row + 4), "transient_rel_abund"]
-  pulses$transientT5[i] <- combined_data[as.character(min_row + 5), "transient_rel_abund"]
-  pulses$transientT6[i] <- combined_data[as.character(min_row + 6), "transient_rel_abund"]
-  pulses$transientT7[i] <- combined_data[as.character(min_row + 7), "transient_rel_abund"]
-  pulses$transientT8[i] <- combined_data[as.character(min_row + 8), "transient_rel_abund"]
-  pulses$transientT9[i] <- combined_data[as.character(min_row + 9), "transient_rel_abund"]
-  pulses$transientT10[i] <- combined_data[as.character(min_row + 10), "transient_rel_abund"]
-  pulses$transientT11[i] <- combined_data[as.character(min_row + 11), "transient_rel_abund"]
-  pulses$transientT12[i] <- combined_data[as.character(min_row + 12), "transient_rel_abund"]
+  pulses$transientT0[i] <- combined_data$transient_rel_abund[combined_data$date == min_date]
+  pulses$transientT1[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 1/12)]
+  pulses$transientT2[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 2/12)]
+  pulses$transientT3[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 3/12)]
+  pulses$transientT4[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 4/12)]
+  pulses$transientT5[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 5/12)]
+  pulses$transientT6[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 6/12)]
+  pulses$transientT7[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 7/12)]
+  pulses$transientT8[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 8/12)]
+  pulses$transientT9[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 9/12)]
+  pulses$transientT10[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 10/12)]
+  pulses$transientT11[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 11/12)]
+  pulses$transientT12[i] <- combined_data$transient_rel_abund[combined_data$date == (min_date + 12/12)]
   
 }
 
-pulses$timeBetween[38] <- NA
 
 #========================================================================
 # Run CCA Analysis
 
+# remove row with NAs
+pulses <- pulses[-33,]
+
 # separate into env and response variables
 pulses_NDVI <- pulses[,1:5]
 pulses_transients <- pulses[,6:18]
+
+# remove rows where rowSum is 0
+rsum <- rowSums(pulses_transients)
+pulses_transients <- pulses_transients[-c(29,30),]
 
 # histograms of transient distributions
 mapply(hist, as.data.frame(pulses_transients[,1:13], 
@@ -318,14 +375,38 @@ rTrans <- rTrans[-c(34,35),]
 cv(rowSums(rTrans, na.rm = TRUE))
 cv(colSums(rTrans, na.rm = TRUE))
 
-# replace NAs with blanks
-rTrans[is.na(rTrans)] <- " "
-rTrans <- as.data.frame(lapply(rTrans, as.numeric))
-
 # Determine Response Model (RDA vs CCA)
 decorana(rTrans)
+  # <3 indicates linear, but damn, that's close to three
+  # gonna start with CCA
 
+# scale the explanatory variables
+explanatory <- as.data.frame(scale(pulses_NDVI[-c(29,30), -1]))
 
+# run CCA
+
+ca <- cca(rTrans)
+plot(ca)
+summary(ca)
+
+transient.CCA <- cca(rTrans ~ ., data = explanatory)
+summary(transient.CCA)
+
+anova.cca(transient.CCA)
+anova.cca(transient.CCA, by = 'axis')
+
+par(mfrow = c(1,2))
+plot(transient.CCA$CCA$wa[,1], transient.CCA$CCA$wa[,2], 
+     xlab = "CCA Axis 1", ylab = "CCA Axis 2")
+plot(transient.CCA$CCA$u[,1], transient.CCA$CCA$u[,2], 
+     xlab = "CCA Axis 1", ylab = "CCA Axis 2")
+
+spenvcor(transient.CCA)
+
+transient.CCA$CCA$biplot
+
+plot(transient.CCA, choices=c(1,2), display = c('wa','sp','bp'), scaling = 2)
+plot(transient.CCA, choices=c(1,2), display = c('lc','sp','bp'), scaling = 2)
 
 #====================================================================
 # Working Area #
