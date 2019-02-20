ae7ff93d389a2b9267967351837cc0a822b73352
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2019-02-19 21:47:44 -0500

    madding code for new dataframe and note about maybe using new moons to avoid problems?

--- scripts/transients_heatmap2.R
+++ scripts/transients_heatmap2.R
@@ -5,9 +5,7 @@
 # LIBRARIES and DATA #
 
 library(RCurl)
-library(stringr)
 library(zoo)
-#library(lubridate)
 library(tidyverse)
 
 rdat <- read.csv(text = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent.csv"), na.strings = '')
@@ -109,10 +107,14 @@ trapping_effort <- trapping %>%
 
 # add month, year, and effor to relative transient abundance
 trapping_data <- full_join(trapping_dates, trapping_effort)
-rel_abund_dates <- full_join(trapping_dates, rel_abund_trans)
+rel_abund_data <- full_join(trapping_data, rel_abund_trans)
 
 # double the number of transients if approriate
-
+for (i in 1:nrow(rel_abund_data)){
+  if (rel_abund_data$nplots[i] <= 17){
+    rel_abund_data$transient_rel_abund[i] <- rel_abund_data$transient_rel_abund[i]*2
+  }
+}
 
 #=========================================================
 # COMBINE NDVI AND TRANSIENT DATA
@@ -121,5 +123,53 @@ rel_abund_dates <- full_join(trapping_dates, rel_abund_trans)
 NDVI <- NDVI %>% arrange(year, month)
 
 # add transient relative abundance and yearmon object
-NDVI_transient <- full_join(rel_abund_dates, NDVI)
+NDVI_transient <- full_join(rel_abund_data, NDVI)
 NDVI_transient$date <- as.yearmon(paste(NDVI_transient$year, NDVI_transient$month), "%Y %m")  
+
+################### need to deal with multiple months per period 
+                  # and multiple periods per month
+                  # would using new_moons as the x-axis fix this issue?
+
+#==========================================================
+# MAKE NEW DATAFRAME FOR HEATMAP #
+
+# get median value of NDVI
+NDVI_transient <- mutate(NDVI_transient, NDVI_median_center = ndvi - median(ndvi, na.rm = TRUE))
+
+# make outline of data frame
+nrows <- length(NDVI_transient$date)
+lags <- data.frame(NDVI = NDVI_transient$NDVI_median_center,
+                   d.NDVI = numeric(nrows),
+                   transientT0 = numeric(nrows),
+                   transientT1 = numeric(nrows),
+                   transientT2 = numeric(nrows),
+                   transientT3 = numeric(nrows),
+                   transientT4 = numeric(nrows),
+                   transientT5 = numeric(nrows),
+                   transientT6 = numeric(nrows),
+                   transientT7 = numeric(nrows),
+                   transientT8 = numeric(nrows),
+                   transientT9 = numeric(nrows),
+                   transientT10 = numeric(nrows),
+                   transientT11 = numeric(nrows))
+
+for (i in 2:length(combined_data$date)){
+  
+  # find change in NDVI
+  lags$d.NDVI[i] <- lags$NDVI[i] - lags$NDVI[i-1]
+  
+  # find the transient rel abundances
+  lags$transientT0[i] <- combined_data$transient_rel_abund[i]
+  lags$transientT1[i] <- combined_data$transient_rel_abund[i + 1]
+  lags$transientT2[i] <- combined_data$transient_rel_abund[i + 2]
+  lags$transientT3[i] <- combined_data$transient_rel_abund[i + 3]
+  lags$transientT4[i] <- combined_data$transient_rel_abund[i + 4]
+  lags$transientT5[i] <- combined_data$transient_rel_abund[i + 5]
+  lags$transientT6[i] <- combined_data$transient_rel_abund[i + 6]
+  lags$transientT7[i] <- combined_data$transient_rel_abund[i + 7]
+  lags$transientT8[i] <- combined_data$transient_rel_abund[i + 8]
+  lags$transientT9[i] <- combined_data$transient_rel_abund[i + 9]
+  lags$transientT10[i] <- combined_data$transient_rel_abund[i + 10]
+  lags$transientT11[i] <- combined_data$transient_rel_abund[i + 11]
+  
+}
